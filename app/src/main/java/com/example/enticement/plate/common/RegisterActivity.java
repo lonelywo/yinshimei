96320b5ff65ab3b701bf9123172eddb00f3a79f8
lonelywo <850411893@qq.com>
2019-09-11 22:06:23 +0800

    购物车和订单和登录注册界面等

--- app/src/main/java/com/example/enticement/plate/common/RegisterActivity.java
+++ app/src/main/java/com/example/enticement/plate/common/RegisterActivity.java
@@ -5,10 +5,12 @@ import android.os.Bundle;
 import com.cuci.enticement.R;
 import com.example.enticement.base.BaseActivity;
 
-public class LoginActivity extends BaseActivity {
+public class RegisterActivity extends BaseActivity {
+
+
     @Override
     public int getLayoutId() {
-        return R.layout.activity_login;
+        return R.layout.activity_register;
     }
 
     @Override
