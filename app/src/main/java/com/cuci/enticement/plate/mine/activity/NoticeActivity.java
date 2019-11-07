08fe8f398759af92186ec62aeac24440f071f780
lonelywo <850411893@qq.com>
2019-11-07 18:26:01 +0800

    公告布局

---
+++ app/src/main/java/com/cuci/enticement/plate/mine/activity/NoticeActivity.java
@@ -0,0 +1,18 @@
+package com.cuci.enticement.plate.mine.activity;
+
+import android.os.Bundle;
+
+import com.cuci.enticement.R;
+import com.cuci.enticement.base.BaseActivity;
+
+public class NoticeActivity extends BaseActivity {
+    @Override
+    public int getLayoutId() {
+        return R.layout.activity_notice;
+    }
+
+    @Override
+    public void initViews(Bundle savedInstanceState) {
+
+    }
+}
---
+++ app/src/main/java/com/cuci/enticement/plate/mine/activity/NoticeRecActivity.java
@@ -0,0 +1,18 @@
+package com.cuci.enticement.plate.mine.activity;
+
+import android.os.Bundle;
+
+import com.cuci.enticement.R;
+import com.cuci.enticement.base.BaseActivity;
+
+public class NoticeRecActivity extends BaseActivity {
+    @Override
+    public int getLayoutId() {
+        return R.layout.activity_notice_rec;
+    }
+
+    @Override
+    public void initViews(Bundle savedInstanceState) {
+
+    }
+}
