c5a95f0469607256297b7bab2ee294d3f86118c5
lonelywo <850411893@qq.com>
2019-11-11 18:43:26 +0800

    例行提交

---
+++ app/src/main/java/com/cuci/enticement/plate/mine/activity/TuiDetailsNOActivity.java
@@ -0,0 +1,18 @@
+package com.cuci.enticement.plate.mine.activity;
+
+import android.os.Bundle;
+
+import com.cuci.enticement.R;
+import com.cuci.enticement.base.BaseActivity;
+
+public class TuiDetailsNOActivity extends BaseActivity {
+    @Override
+    public int getLayoutId() {
+        return R.layout.tui_details_no;
+    }
+
+    @Override
+    public void initViews(Bundle savedInstanceState) {
+
+    }
+}
