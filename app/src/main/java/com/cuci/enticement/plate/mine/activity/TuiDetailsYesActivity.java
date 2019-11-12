d469864cca1ac7c5dd45a3146d0d58707fc33408
lonelywo <850411893@qq.com>
2019-11-12 18:40:21 +0800

    例行提交

---
+++ app/src/main/java/com/cuci/enticement/plate/mine/activity/TuiDetailsYesActivity.java
@@ -0,0 +1,18 @@
+package com.cuci.enticement.plate.mine.activity;
+
+import android.os.Bundle;
+
+import com.cuci.enticement.R;
+import com.cuci.enticement.base.BaseActivity;
+
+public class TuiDetailsYesActivity extends BaseActivity {
+    @Override
+    public int getLayoutId() {
+        return R.layout.tui_details_yes;
+    }
+
+    @Override
+    public void initViews(Bundle savedInstanceState) {
+
+    }
+}
