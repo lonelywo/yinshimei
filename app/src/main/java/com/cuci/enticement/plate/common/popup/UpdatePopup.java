eb7f74981ff27fffc425005a9fed9a2cfb647787
lonelywo <850411893@qq.com>
2020-04-10 11:37:11 +0800

    版本框

--- app/src/main/java/com/cuci/enticement/plate/common/popup/UpdatePopup.java
+++ app/src/main/java/com/cuci/enticement/plate/common/popup/UpdatePopup.java
@@ -3,10 +3,10 @@ package com.cuci.enticement.plate.common.popup;
 import android.content.Context;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-
 
 import com.cuci.enticement.R;
 import com.cuci.enticement.bean.Version;
@@ -20,13 +20,21 @@ import butterknife.OnClick;
 
 public class UpdatePopup extends CenterPopupView {
 
-    @BindView(R.id.content)
-    TextView mContent;
+
+    @BindView(R.id.top)
+    ImageView top;
     @BindView(R.id.close)
     ImageView mClose;
+    @BindView(R.id.content)
+    TextView mContent;
     @BindView(R.id.ignore)
     TextView mIgnore;
-
+    @BindView(R.id.ll)
+    LinearLayout ll;
+    @BindView(R.id.line)
+    View line;
+    @BindView(R.id.update)
+    TextView update;
     private Context mContext;
     private Version mVersion;
 
@@ -49,7 +57,7 @@ public class UpdatePopup extends CenterPopupView {
 
         mContent.setText(mVersion.getData().getContent());
 
-        if (mVersion.getData().getIs_force()==1) {
+        if (mVersion.getData().getIs_force() == 1) {
             ViewUtils.hideView(mClose);
             ViewUtils.hideView(mIgnore);
         }
