e2b1e136f3fecb7742d664651db354f540a8b42c
lonelywo <850411893@qq.com>
2020-04-30 18:26:06 +0800

    tips

--- app/src/main/java/com/cuci/enticement/plate/common/MainActivity.java
+++ app/src/main/java/com/cuci/enticement/plate/common/MainActivity.java
@@ -470,7 +470,8 @@ public class MainActivity extends AppCompatActivity implements TipsPopupxieyi.On
                                             .asCustom(new UpdateProgressPopup(this, mData))
                                             .show();
                                 }*/
-                               FToast.warning("请前往应用商店下载最新版本");
+                             //  FToast.warning("请前往手机应用商店下载最新版本app");
+                                launchAppDetail("com.cuci.enticement","");
                             }))
                     .show();
         }
@@ -619,6 +620,26 @@ public class MainActivity extends AppCompatActivity implements TipsPopupxieyi.On
     }
 
 
-
+    /**
+     * 启动到应用商店app详情界面
+     *
+     * @param appPkg  目标App的包名
+     * @param marketPkg 应用商店包名,如果为""则由系统弹出应用商店列表供用户选择,否则调转到目标市场的应用详情界面，某些应用商店可能会失败
+     */
+    public void launchAppDetail(String appPkg,String marketPkg) {
+        try {
+            if (TextUtils.isEmpty(appPkg)) return;
+            Uri uri = Uri.parse("market://details?id=" + appPkg);
+            Intent intent = new Intent(Intent.ACTION_VIEW,uri);
+            if (!TextUtils.isEmpty(marketPkg)) {
+                intent.setPackage(marketPkg);
+            }
+          //  FToast.warning("请前往手机应用商店下载最新版本app");
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
 }
